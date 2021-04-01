f045d92de75d7c0e345bd8e8d0410464d64b9b94
Clara Guo <guoguo.clara@gmail.com>
2021-03-31 23:31:58 -0400

    Added LSTM_Skeletal_Features_Dataset

--- dataset.py
+++ dataset.py
@@ -154,12 +154,66 @@ class LSTM_Dataset_Wrapper(LSTM_Dataset):
 
 
 
+class LSTM_Skeletal_Features_Dataset(data.Dataset):
+    "Characterizes a dataset for PyTorch"
+    def __init__(self, config, inputs, labels, max_frames):
+        "Initialization"
+        self.config = config
+        self.labels = labels
+        self.inputs = inputs
+        self.max_frames = max_frames
+        self.skeletal_features_path = config.get('dataset', 'skeletal_features_all_timestamps_path')
+        self.num_features = config.getint('lstm', 'n_features')
+
+    def __len__(self):
+        "Denotes the total number of samples"
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        "Generates one sample of data"
+        # Select sample
+        input = self.inputs[index]
+
+        # Load data from the text file
+        txt_file_name = os.path.join(*(input.split('/')[-6:])).replace("/", "_").split(".")[0]
+        if (txt_file_name[-1] == "_"):
+            # TODO: need a better fix
+            # Naming is inconsistent in KIMORE, some videos has an extra underscore. The extra underscore needs to be removed
+            # i.e. 'CG_Expert_E_ID9_Es1_rgb_Blur_rgb271114_123334_'
+            txt_file_name = txt_file_name[:-1]
+
+        data = np.loadtxt(os.path.join(self.skeletal_features_path, txt_file_name + ".txt"), delimiter=',')
+        data = data[:, :self.num_features]
+        # Convert numpy array to tensor
+        X = torch.from_numpy(data)
+
+        X_len = X.shape[0]         # Number of frames
+
+        # The needed padding is the difference between the X.shape[0] and max_frames.
+        p1d = (0, 0, 0, self.max_frames - X_len)
+        X = F.pad(X, p1d, "constant", 0)                           # [max_frames, n_joints]
+
+        y = torch.LongTensor([self.labels[index]])               # (labels) LongTensor are for int64 instead of FloatTensor
+
+        # label = self.labels[index]                                 # (label) clinical score
+        # y = torch.tensor(())
+        # if (label == 1):
+        #     y = y.new_ones((X.shape[0], 1), dtype=torch.long)            # [max_frames, 1]
+        # else:
+        #     y = y.new_zeros((X.shape[0], 1), dtype=torch.long)           # [max_frames, 1]
+        return X, y, X_len
+
+
+
+## ---------------------- MLP Dataloader ---------------------- ##
+
 class MLP_Dataset(data.Dataset):
 
     def __init__(self, X, y, config):
         self.features = X
         self.label = y
         self.skeletal_features_path = config.get('dataset', 'skeletal_features_path')
+        self.num_features = config.getint('mlp', 'n_features')
 
     def __len__(self):
         return len(self.features)
@@ -178,7 +232,7 @@ class MLP_Dataset(data.Dataset):
 
         data = np.loadtxt(os.path.join(self.skeletal_features_path, txt_file_name + ".txt"), delimiter=',')
 
-        data = data[:, :3]
+        data = data[:, :self.num_features]
         # Convert numpy array to tensor
         X = torch.from_numpy(data)
         # Flatten X

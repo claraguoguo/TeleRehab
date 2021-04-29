23bdbac669a21fe4e7d45513d4683cdc9b2ea590
Clara Guo <guoguo.clara@gmail.com>
2021-04-29 14:15:50 -0400

    Updated MLP model to have 1 hidden layer

--- models/NN_models.py
+++ models/NN_models.py
@@ -1,18 +1,24 @@
 import torch.nn as nn
-
+import torch
 class MLP(nn.Module):
   '''
     Multilayer Perceptron.
   '''
 
   def __init__(self, input_size):
-      super(MLP, self).__init__()
-      self.input_size = input_size
-      self.output_size = 1
+    super(MLP, self).__init__()
+    self.input_size = input_size
+    self.output_size = 1
 
-      self.fc1 = nn.Linear(self.input_size, 64)
-      self.fc2 = nn.Linear(64, 32)
-      self.fc3 = nn.Linear(32, self.output_size)
+    # self.fc1 = nn.Linear(self.input_size, 64)
+    # self.fc2 = nn.Linear(64, 32)
+    # self.fc3 = nn.Linear(32, self.output_size)
+
+    hidden_size = (self.input_size + self.output_size)//2
+
+    self.fc1 = nn.Linear(self.input_size, hidden_size)
+    self.fc3 = nn.Linear(hidden_size, self.output_size)
+
 
   def forward(self, features):
       ###### 3 Hidden Layers
@@ -30,8 +36,8 @@ class MLP(nn.Module):
       x = self.fc1(features.float())
       x = act_func(x)
 
-      x = self.fc2(x)
-      x = act_func(x)
+      # x = self.fc2(x)
+      # x = act_func(x)
 
       x = self.fc3(x)
       return x
